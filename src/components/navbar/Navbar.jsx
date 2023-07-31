14bfc2f98a17811941dfce4356f530e730ac1ae1
zanetowell <zanetowell@gmail.com>
2023-07-31 15:53:22 -0400

    Adds light/dark mode toggle

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import React from 'react'
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle'
 import styles from './page.module.css'
 
 const Navbar = () => {
@@ -43,6 +44,7 @@ const Navbar = () => {
     <div className={styles.container}>
         <Link href='/' className={styles.logo}>Zane Corp</Link>
         <div className={styles.links}>
+          <DarkModeToggle />
             {links.map(link=>(
                 <Link key={link.id} href={link.url} className={styles.link}>
                     {link.title}
